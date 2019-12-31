53bb32bfb91ce5d2c16fadb3d238da27211dfe3d
Jeff Frazier <jeff@black-bear.io>
2019-12-31 14:21:15 -0500

    Add a background image for Settings screen

--- lib/settings.dart
+++ lib/settings.dart
@@ -34,104 +34,108 @@ class _SettingsState extends State<Settings> {
         Bike $bike = bikes[index];
         var fork = $bike.fork ?? null;
         var shock = $bike.shock ?? null;
-        return Dismissible(
-          background: ListTile(
-            trailing: Icon(Icons.delete, color: CupertinoColors.systemRed),
-          ),
-          direction: DismissDirection.endToStart,
-          onDismissed: (direction) => setState(() {
-            bikes.removeAt(index);
-            db.deleteBike(uid, $bike.id);
-          }),
-          key: PageStorageKey($bike),
-          child: ExpansionTile(
-            initiallyExpanded: index == 0 ? true : false,
+        return Container(
+          margin: EdgeInsets.only(bottom: 10),
+          child: Dismissible(
+            background: ListTile(
+              trailing: Icon(Icons.delete, color: CupertinoColors.systemRed),
+            ),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) => setState(() {
+              bikes.removeAt(index);
+              db.deleteBike(uid, $bike.id);
+            }),
             key: PageStorageKey($bike),
-            title: Text($bike.id),
-            children: <Widget>[
-              fork != null ? 
-                GestureDetector(
-                  child: ListTile(
-                    leading: Image.asset('assets/fox36-black.jpg', height: 40),
-                    title: Text(fork["year"].toString() + ' ' + fork["brand"] + ' ' + fork["model"]),
-                    subtitle: Text(fork["travel"].toString() + 'mm / ' + fork["damper"] + ' / ' + fork["offset"].toString() + 'mm / ' + fork["wheelsize"].toString() + '"'),
-                  ),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      CupertinoPageRoute(
-                        fullscreenDialog: true,
-                        builder: (context) {
-                          return CupertinoPageScaffold(
-                            resizeToAvoidBottomInset: true,
-                            navigationBar: CupertinoNavigationBar(
-                              middle: Text(fork != null ? fork['brand'] + ' ' + fork['model'] : 'Add fork'),
-                            ),
-                            child: ForkForm(uid: uid, bike: $bike.id, fork: fork),
-                          );
-                        }
-                      ),
-                    );
-                  }
-                )
-                : ListTile(
-                    leading: Image.asset('assets/fox36-black.jpg', height: 35),
-                    title: Text('No fork info'),
-                  ),
-                  // If shock data exists populate info and link to settings.
-                  shock != null ?
+            child: ExpansionTile(
+              initiallyExpanded: index == 0 ? true : false,
+              key: PageStorageKey($bike),
+              title: Text($bike.id),
+              children: <Widget>[
+                fork != null ? 
                   GestureDetector(
                     child: ListTile(
-                      leading: Image.asset('assets/fox-dpx2.png', height: 40),
-                      title: Text(shock["year"].toString() + ' ' + shock["brand"] + ' ' + shock["model"]),
-                      subtitle: Text(shock["stroke"] ?? ''),
+                      leading: Image.asset('assets/fox36-black.jpg', height: 40),
+                      title: Text(fork["year"].toString() + ' ' + fork["brand"] + ' ' + fork["model"]),
+                      subtitle: Text(fork["travel"].toString() + 'mm / ' + fork["damper"] + ' / ' + fork["offset"].toString() + 'mm / ' + fork["wheelsize"].toString() + '"'),
                     ),
                     onTap: () {
                       Navigator.of(context).push(
                         CupertinoPageRoute(
                           fullscreenDialog: true,
                           builder: (context) {
-                            // var $bike = widget.bike;
-                            // var $shock = widget.shock;
                             return CupertinoPageScaffold(
                               resizeToAvoidBottomInset: true,
                               navigationBar: CupertinoNavigationBar(
-                                middle: Text($bike != null ? shock['brand'] + ' ' + shock['model'] : ''),
+                                middle: Text(fork != null ? fork['brand'] + ' ' + fork['model'] : 'Add fork'),
                               ),
-                              child: ShockForm(uid: uid, bike: $bike.id, shock: shock),
+                              child: ForkForm(uid: uid, bike: $bike.id, fork: fork),
                             );
-                          },
-                        )
+                          }
+                        ),
                       );
                     }
-                  ) 
-                : ListTile(
-                    leading: Image.asset('assets/fox-dpx2.png', height: 35),
-                    title: Text('No shock info'),
-                  ),
-                  GestureDetector(
-                    child: ListTile(
-                      leading: Icon(CupertinoIcons.settings),
-                      title: Text('Suspension Settings'),
-                      trailing: Icon(Icons.arrow_forward_ios),
+                  )
+                  : ListTile(
+                      leading: Image.asset('assets/fox36-black.jpg', height: 35),
+                      title: Text('No fork info'),
                     ),
-                    onTap: () {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(builder: (context) {
-                          // Return the shock detail form screen here.
-                          return SettingsList(bike: $bike);
-                        })
-                      );
-                    }
-                  ),
-                ],
-              ),
-          );
+                    // If shock data exists populate info and link to settings.
+                    shock != null ?
+                    GestureDetector(
+                      child: ListTile(
+                        leading: Image.asset('assets/fox-dpx2.png', height: 40),
+                        title: Text(shock["year"].toString() + ' ' + shock["brand"] + ' ' + shock["model"]),
+                        subtitle: Text(shock["stroke"] ?? ''),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            fullscreenDialog: true,
+                            builder: (context) {
+                              // var $bike = widget.bike;
+                              // var $shock = widget.shock;
+                              return CupertinoPageScaffold(
+                                resizeToAvoidBottomInset: true,
+                                navigationBar: CupertinoNavigationBar(
+                                  middle: Text($bike != null ? shock['brand'] + ' ' + shock['model'] : ''),
+                                ),
+                                child: ShockForm(uid: uid, bike: $bike.id, shock: shock),
+                              );
+                            },
+                          )
+                        );
+                      }
+                    ) 
+                  : ListTile(
+                      leading: Image.asset('assets/fox-dpx2.png', height: 35),
+                      title: Text('No shock info'),
+                    ),
+                    GestureDetector(
+                      child: ListTile(
+                        leading: Icon(CupertinoIcons.settings),
+                        title: Text('Suspension Settings'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(builder: (context) {
+                            // Return the shock detail form screen here.
+                            return SettingsList(bike: $bike);
+                          })
+                        );
+                      }
+                    ),
+                  ],
+                ),
+            ),
+        );
         },
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     var user = Provider.of<FirebaseUser>(context);
     if (user == null) {
       return Center(
@@ -155,50 +159,70 @@ class _SettingsState extends State<Settings> {
           navigationBar: CupertinoNavigationBar(
             middle: Text('Settings'),
             trailing: CupertinoButton(
+              padding: EdgeInsets.only(bottom: 0),
               child: Icon(Icons.power_settings_new),
               onPressed: () => _requestPop(context)
             ),
           ),
-          child: Material(
-            child: ListView(
-              // reverse: true,
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              children: <Widget>[
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    StreamBuilder<List<Bike>>(
-                      stream: db.streamBikes(user.uid),
-                      builder: (context, snapshot) {
-                        var bikes = snapshot.data;
-                        if (bikes == null) {
-                          return Center(
-                            child: Text('Loading...',
-                            style: CupertinoTheme.of(context).textTheme.navTitleTextStyle),
-                          );
+          child: AnimatedContainer(
+            key: Key('settings'),
+            duration: Duration(milliseconds: 250),
+            curve: Curves.fastOutSlowIn,
+            height: height,
+            padding: EdgeInsets.fromLTRB(10, 160, 10, 10),
+            decoration: BoxDecoration(
+              // backgroundBlendMode: BlendMode.softLight,
+              color: Colors.grey,
+              image: DecorationImage(
+                image: AssetImage("assets/roost.jpg"),
+                fit: BoxFit.contain,
+                alignment: Alignment.topLeft
+              ),
+            ),
+            child: Card(
+              color: Colors.white70,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16))
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      StreamBuilder<List<Bike>>(
+                        stream: db.streamBikes(user.uid),
+                        builder: (context, snapshot) {
+                          var bikes = snapshot.data;
+                          if (bikes == null) {
+                            return Center(
+                              child: Text('Loading...',
+                              style: CupertinoTheme.of(context).textTheme.navTitleTextStyle),
+                            );
+                          }
+                          return _getBikes(user.uid, bikes, context);
                         }
-                        return _getBikes(user.uid, bikes, context);
-                      }
-                    ),
-                    SizedBox(height: 20),
-                    CupertinoButton(
-                      color: CupertinoColors.systemFill,
-                      child: Text('Add Bike'),
-                      onPressed: () => Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          fullscreenDialog: true, // loads form from bottom
-                          builder: (context) {
-                          // We need to return the shock detail screen here.
-                          return BikeForm(uid: user.uid);
-                        })
                       ),
-                    ),
-                    // Expanded(child: Container())
-                  ],
-                ),
-              ],
+                      SizedBox(height: 20),
+                      CupertinoButton(
+                        color: CupertinoColors.systemFill,
+                        child: Text('Add Bike'),
+                        onPressed: () => Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            fullscreenDialog: true, // loads form from bottom
+                            builder: (context) {
+                            // We need to return the shock detail screen here.
+                            return BikeForm(uid: user.uid);
+                          })
+                        ),
+                      ),
+                      // Expanded(child: Container())
+                    ],
+                  ),
+                ],
+              ),
             ),
           )
         );
