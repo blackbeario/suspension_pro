17f5f1fa474aa0674d8d34ed00038a80446779de
Jeff Frazier <jfrazdev@gmail.com>
2025-01-03 14:14:34 -0500

    Rm .flutter-plugins-dependencies

---
+++ lib/services/hive_service.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+class HiveService {
+  void putIntoBox(String boxName, String key, dynamic object) async {
+    try {
+      final box = await Hive.openBox(boxName);
+      await box.put(key, object);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  getFromBox(String boxName, String key) async {
+    try {
+      final box = await Hive.openBox(boxName);
+      return await box.get(key);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
